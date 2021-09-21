547ecd5d3d344e864b2dbad8f29384060c6e5bc8
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2021-09-21 14:41:26 +0300

    fixed layout spacing issues

--- src/Components/Layout/ContentLayout/ContentLayout.jsx
+++ src/Components/Layout/ContentLayout/ContentLayout.jsx
@@ -1,7 +1,16 @@
+import PlusCircled from ".././../../Assets/plus-circle.svg";
 import "./ContentLayout.scss";
 
 const ContentLayout = () => {
-  return <div className="col col-lg-10"></div>;
+  return (
+    <div className="col col-lg-10 content-layout">
+      <div className="card card-content-layout">
+        <div class="add-graph-button">
+          <img src={PlusCircled} alt="add graph" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContentLayout;
--- src/Components/Layout/SideLayout/SideLayout.jsx
+++ src/Components/Layout/SideLayout/SideLayout.jsx
@@ -3,10 +3,23 @@ import "./SideLayout.scss";
 const SideLayout = (props) => {
   const { list } = props;
 
+  // const handleSelectedItem = (e) => {
+  //   console.log(e.target.id);
+  //   const selectedElementNode = document.getElementById(e.target.id);
+  //   selectedElementNode.classList.toggle("active");
+  // };
+
   const selectionList = (
-    <div className="list-group list-group-flush">
+    <div className="list-group list-group-flush" role="tablist">
       {list.map((item) => (
-        <button key={item} className="list-group-item list-group-item-action">
+        <button
+          key={`${item}-item`}
+          id={`list-${item}-list`}
+          data-bs-toggle="list"
+          role="tab"
+          // onClick={handleSelectedItem}
+          className="list-group-item list-group-item-action"
+        >
           {item}
         </button>
       ))}
@@ -14,8 +27,10 @@ const SideLayout = (props) => {
   );
 
   return (
-    <div className="col col-lg-2 card ">
-      <div className="card-body">{selectionList}</div>
+    <div className="col col-lg-2 sidelayout ">
+      <div className="card card-sidelayout">
+        <div className="card-body card-sidelayout-body">{selectionList}</div>
+      </div>
     </div>
   );
 };
